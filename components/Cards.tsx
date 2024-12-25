01464c9acb92a47a01c7a98d81a37b7bcbdd251b
geekyharsh05 <hp424805@gmail.com>
2024-12-25 21:38:17 +0530

    Refactor Filters and Cards components to utilize dynamic data for property details and improve UI responsiveness

--- components/Cards.tsx
+++ components/Cards.tsx
@@ -1,44 +1,56 @@
-import { View, Text, Touchable, TouchableOpacity, Image } from 'react-native'
-import React from 'react'
-import images from '@/constants/images'
-import icons from '@/constants/icons'
+import icons from "@/constants/icons";
+import images from "@/constants/images";
+import { Image, Text, TouchableOpacity, View } from "react-native";
+import { Models } from "react-native-appwrite";
 
 interface iProps {
-    onPress?: () => void
+    item: Models.Document;
+    onPress?: () => void;
 }
 
-export const FeaturedCard = ({ onPress }: iProps) => {
+export const FeaturedCard = ({ item, onPress }: iProps) => {
     return (
-        <TouchableOpacity onPress={onPress} className='flex flex-col items-start w-60 h-80 relative'>
-            <Image
-                source={images.japan}
-                className='size-full rounded-2xl'
-            />
+        <TouchableOpacity
+            onPress={onPress}
+            className="flex flex-col items-start w-60 h-80 relative"
+        >
+            <Image source={{ uri: item.image }} className="size-full rounded-2xl" />
+
             <Image
                 source={images.cardGradient}
-                className='size-full rounded-2xl absolute bottom-0'
+                className="size-full rounded-2xl absolute bottom-0"
             />
 
             <View className="flex flex-row items-center bg-white/90 px-3 py-1.5 rounded-full absolute top-5 right-5">
                 <Image source={icons.star} className="size-3.5" />
                 <Text className="text-xs font-rubik-bold text-primary-300 ml-1">
-                    4.3
+                    {item.rating}
                 </Text>
             </View>
 
-            <View className='flex flex-col items-start absolute bottom-5 inset-x-5'>
-                <Text className='text-xl text-white font-rubik-extrabold' numberOfLines={1}>Modern Apartment</Text>
-                <Text className='text-base font-rubik text-white'>22W 15th St, New York</Text>
-                <View className='flex flex-row items-center justify-between w-full'>
-                    <Text className='text-xl font-rubik-extrabold text-white'>$2,500</Text>
-                    <Image source={icons.heart} className='size-5' />
+            <View className="flex flex-col items-start absolute bottom-5 inset-x-5">
+                <Text
+                    className="text-xl font-rubik-extrabold text-white"
+                    numberOfLines={1}
+                >
+                    {item.name}
+                </Text>
+                <Text className="text-base font-rubik text-white" numberOfLines={1}>
+                    {item.address}
+                </Text>
+
+                <View className="flex flex-row items-center justify-between w-full">
+                    <Text className="text-xl font-rubik-extrabold text-white">
+                        ${item.price}
+                    </Text>
+                    <Image source={icons.heart} className="size-5" />
                 </View>
             </View>
         </TouchableOpacity>
-    )
-}
+    );
+};
 
-export const Card = ({ onPress }: iProps) => {
+export const Card = ({ item, onPress }: iProps) => {
     return (
         <TouchableOpacity
             className="flex-1 w-full mt-4 px-3 py-4 rounded-lg bg-white shadow-lg shadow-black-100/70 relative"
@@ -47,23 +59,23 @@ export const Card = ({ onPress }: iProps) => {
             <View className="flex flex-row items-center absolute px-2 top-5 right-5 bg-white/90 p-1 rounded-full z-50">
                 <Image source={icons.star} className="size-2.5" />
                 <Text className="text-xs font-rubik-bold text-primary-300 ml-0.5">
-                    4.4
+                    {item.rating}
                 </Text>
             </View>
 
-            <Image source={images.newYork} className="w-full h-40 rounded-lg" />
+            <Image source={{ uri: item.image }} className="w-full h-40 rounded-lg" />
 
             <View className="flex flex-col mt-2">
                 <Text className="text-base font-rubik-bold text-black-300">
-                    Modern Apartment
+                    {item.name}
                 </Text>
                 <Text className="text-xs font-rubik text-black-100">
-                    22W 15th St, New York
+                    {item.address}
                 </Text>
 
                 <View className="flex flex-row items-center justify-between mt-2">
                     <Text className="text-base font-rubik-bold text-primary-300">
-                        $2,500
+                        ${item.price}
                     </Text>
                     <Image
                         source={icons.heart}
@@ -74,4 +86,4 @@ export const Card = ({ onPress }: iProps) => {
             </View>
         </TouchableOpacity>
     );
-};  
+};
--- components/Filters.tsx
+++ components/Filters.tsx
@@ -22,6 +22,7 @@ const Filters = () => {
         <ScrollView horizontal showsHorizontalScrollIndicator={false} className='mt-3 mb-2'>
             {categories.map((item, index) => (
                 <TouchableOpacity
+                    key={index}
                     onPress={() => handleCategory(item.category)}
                     className={`flex flex-col items-start mr-4 px-4 py-2 rounded-full ${selectedCategory === item.category ? "bg-primary-300" : "bg-primary-100 border border-primary-200"}`}>
                     <Text className={`text-sm ${selectedCategory === item.category ? "text-white font-rubik-bold mt-0.5" : "text-black-300 font-rubik"}`}>{item.title}</Text>
